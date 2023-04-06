3b9c7e15c7b3c932da4df3de2ffa2d28146c971d
anji907 <anji.sk129@gmail.com>
2023-04-06 16:58:07 +0900

    On branch user-login insert
    t

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+
   root to: "tasks#index"
   resources :tasks
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
